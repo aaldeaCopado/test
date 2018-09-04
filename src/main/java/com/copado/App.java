4639f4dd4fe6586cf5a91ee7c3c5c85dbadfca39
Skynetronic <nibble2k@gmail.com>
2018-09-04 12:59:22 +0200

    Only 4 testing

---
+++ src/main/java/com/copado/App.java
@@ -0,0 +1,13 @@
+package com.copado;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
